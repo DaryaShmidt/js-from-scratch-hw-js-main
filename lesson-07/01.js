68adc7f7c13c9009e283749b012992d0f4b1fa31
daryashmidt <daryabulyha@gmail.com>
2026-04-06 16:37:29 +0300

    lesson-07/01.js

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -17,3 +17,20 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+let img = document.querySelector('#web-tech-image')
+let currentIndex = 0;
+img.src = WEB_TECH_IMAGES[currentIndex];
+
+let next = document.querySelector('#next-button');
+next.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1 + WEB_TECH_IMAGES.length) % WEB_TECH_IMAGES.length;
+  img.src = WEB_TECH_IMAGES[currentIndex];
+})
+
+let prev = document.querySelector('#prev-button'); 
+prev.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + WEB_TECH_IMAGES.length) % WEB_TECH_IMAGES.length;
+  img.src = WEB_TECH_IMAGES[currentIndex];
+})
+
